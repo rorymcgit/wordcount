e00795d6c504352ba7a958e3b98e3b39e5959855
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-06-12 22:51:37 +0100

    Create array of objects in counter

--- spec/counter_test.js
+++ spec/counter_test.js
@@ -5,14 +5,12 @@ const counter = require("../src/counter");
 
 describe("Count words", () => {
   it("returns an object", () => {
-    expect(counter([])).to.eql({});
-    // expect(counter([])).to.eql([{}]);
+    expect(counter([])).to.eql([]);
   });
 
   it("counts recurring words", () => {
     var testWords = ["the", "meerkat", "is", "the", "fastest"];
-    // var expectedObject = [ { "word" : "the", "count" : 2 }, { "word" : "meerkat", "count" : 1 }, { "word" : "is", "count" : 1 }, { "word" : "fastest", "count" : 1 } ];
-    var expectedObject = { "the" : 2, "meerkat" : 1, "is" : 1, "fastest" : 1 };
+    var expectedObject = [ { "word" : "the", "count" : 2, "isPrime" : true }, { "word" : "meerkat", "count" : 1, "isPrime" : false }, { "word" : "is", "count" : 1, "isPrime" : false }, { "word" : "fastest", "count" : 1, "isPrime" : false } ];
     expect(counter(testWords)).to.eql(expectedObject);
   })
 });
--- src/counter.js
+++ src/counter.js
@@ -1,15 +1,30 @@
 "use strict";
 
+const isPrime = require("./isPrime");
+
 function counter(words) {
-  return words.reduce(function(allWords, word) {
-    if (word in allWords) {
-      allWords[word]++;
-    }
-    else {
-      allWords[word] = 1;
-    }
-    return allWords;
-  }, {});
+  var compressed = [];
+	var copy = words.slice(0);
+
+	for (var i = 0; i < words.length; i++) {
+		var myCount = 0;
+		for (var w = 0; w < copy.length; w++) {
+			if (words[i] == copy[w]) {
+				myCount++;
+				delete copy[w];
+			}
+		}
+
+		if (myCount > 0) {
+			var a = new Object();
+			a.word = words[i];
+			a.count = myCount;
+      a.isPrime = isPrime(myCount);
+			compressed.push(a);
+		}
+	}
+  // console.log(compressed);
+	return compressed;
 }
 
 module.exports = counter;
